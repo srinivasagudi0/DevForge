dcce321ae68b4d88ad8346301b1e4321f7213355
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-07-08 11:33:36 -0500

    desgigrd a bckground and aplied

--- frontend/src/project_blueprint.jsx
+++ frontend/src/project_blueprint.jsx
@@ -3,11 +3,16 @@ function ProjectBlueprint() {
     <div>
       <h1 className="title1">Project Blueprint</h1>
       <p className="caption">This is the Project Blueprint page of the application.</p>
-      <div>
-        
-      </div>
+      <select className="dropdown">
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+
     </div>
   )
 }
 
-export default ProjectBlueprint; 
+export default ProjectBlueprint;
+
+// first style the page then work on functionallity
