717653b491f0fb838f5a02dd02d8ab36ae52e781
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-07-08 12:50:39 -0500

    trying to maekt he UI really good and made a ltot progreesss on UI. andw ill l soon starrt working on functionality

--- frontend/src/project_blueprint.jsx
+++ frontend/src/project_blueprint.jsx
@@ -1,12 +1,17 @@
 function ProjectBlueprint() {
   return (
     <div>
+      <p className="icon">≣</p>
       <h1 className="title1">Project Blueprint</h1>
-      <p className="caption">This is the Project Blueprint page of the application.</p>
+      <p className="caption">Start your project the right way. Choose a template and generate a <br/>professional, production-ready structure in seconds.</p>
+      <br/>
+      <div className="caption">
+        <p>🛡️ Best Practices &emsp; &emsp; 🚀  Production Ready &emsp; &emsp; 🧩 Fully Customizable </p>
+      </div>
       <select className="dropdown">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
+        <option value="option1">React</option>
+        <option value="option2">Python</option>
+        <option value="option3">JavaScript</option>
       </select>
 
     </div>
