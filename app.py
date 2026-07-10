9be0fe48c1d4df5ce309f2804433963079164ce5
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-07-10 13:28:17 -0500

    add a background image and nav sidebar

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import base64
+import base64 as b
 
 import streamlit as st
 
 def render_css():
     with open("image.png", "rb") as image:
-        encoded_image = base64.b64encode(image.read()).decode()
+        encoded_image = b.b64encode(image.read()).decode()
 
     with open("style.css") as css:
         styles = css.read().replace("IMAGE_DATA", encoded_image)
@@ -16,8 +16,21 @@ render_css()
 
 st.set_page_config(page_title="DevForge", page_icon=":rocket:", layout="wide")
 
-st.title("DevForge")
 
-st.caption("Welcome to DevForge! This is a platform for developers to collaborate, share ideas, and build amazing projects together.")
+st.sidebar.header("Navigation")
+nav = st.sidebar.selectbox("Go to", ["Home", "Project Blueprint"])
 
 
+
+if nav == "Home":
+    st.title("DevForge")
+    st.caption("Welcome to DevForge! This is a platform for developers that build projects and want to automate annoying tasks.")
+    st.header("Features")
+    st.markdown("""
+    - Project Blueprint: Choose a template and customize it to kickstart your project.
+                """)
+
+if nav == "Project Blueprint":
+    st.title("Project Blueprint")
+    st.caption("Select a template and customize it to create your project structure.")
+    st.caption("This feature is currently under development. Please check back later for updates.")
